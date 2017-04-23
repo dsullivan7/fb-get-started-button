8f0516066e418855a17fda91b28c3fce662d47e1
dsullivan7 <dbsullivan23@gmail.com>
2017-04-23 13:09:47 +1000

    adding babel functionality

--- bin/fb-get-started-button.js
+++ bin/fb-get-started-button.js
@@ -1,1 +1,20 @@
 #! /usr/bin/env node
+
+var argv = require('yargs').argv;
+var lib = require('../lib/index.js')
+
+if (argv.add && argv._.length === 1){
+	console.log(argv._[0])
+}
+else if (argv.remove && argv._.length === 1){
+	console.log(argv._[0])
+}
+else {
+	console.log(`
+Usage:
+
+	fb-get-started-button add|remove <PAGE ACCESS TOKEN>
+
+This will add a "Get Started" button with a "GET_STARTED" payload to the page with the given access token
+`)
+}
