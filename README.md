aa4dac0fdbeca7cfac23c531f94750c6e8658535
dsullivan7 <dbsullivan23@gmail.com>
2017-04-23 16:00:58 +1000

    adding full functionality

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## Usage
 ```
 $ npm install -g fb-get-started-button
-$ fb-get-started-button --add <YOUR PAGE ACCESS TOKEN>
+$ fb-get-started-button add <YOUR PAGE ACCESS TOKEN>
 "Get Started" button successfully added with the payload "GET_STARTED"
-$ fb-get-started-button --remove <YOUR PAGE ACCESS TOKEN>
+$ fb-get-started-button remove <YOUR PAGE ACCESS TOKEN>
 "Get Started" button successfully removed
 ```
--- bin/fb-get-started-button.js
+++ bin/fb-get-started-button.js
@@ -1,20 +1,31 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
-var argv = require('yargs').argv;
-var lib = require('../lib/index.js')
+const argv = require('yargs').argv
+const lib = require('../lib/index.js')
 
-if (argv.add && argv._.length === 1){
-	console.log(argv._[0])
-}
-else if (argv.remove && argv._.length === 1){
-	console.log(argv._[0])
-}
-else {
-	console.log(`
+const payload = argv.payload || 'GET_STARTED'
+
+function usage() {
+  console.log(`
 Usage:
 
-	fb-get-started-button add|remove <PAGE ACCESS TOKEN>
+  fb-get-started-button add|remove <PAGE ACCESS TOKEN> [--payload <CUSTOM PAYLOAD>]
 
-This will add a "Get Started" button with a "GET_STARTED" payload to the page with the given access token
+This will add a "Get Started" button with the given payload to the page with the given access token
+If no payload is specified, the payload will be "GET_STARTED"
 `)
+}
+
+if (argv._.length === 2) {
+  if (argv._[0] === 'add') {
+    console.log(`Adding "Get Started" button with the payload "${payload}"`)
+    lib.addGetStarted(argv._[1], payload)
+  } else if (argv._[0] === 'remove') {
+    console.log('Removing "Get Started" button')
+    lib.removeGetStarted(argv._[1])
+  } else {
+    usage()
+  }
+} else {
+  usage()
 }
